2cbed916c8f1e808430e3a68e950c1e53a8be873
aoxolotl <53764708+aoxolotl@users.noreply.github.com>
2023-08-21 16:04:31 -0500

    Removed dot from dictionary keys

--- entry.py
+++ entry.py
@@ -123,7 +123,7 @@ def send_request(annotation_id=None):
                 # use a from_bbox method
                 # handles merging or creating annotations internally
                 top_left = (bbox["xmin"], bbox["ymin"])
-                bottom_right = (bbox.["xmax"], bbox.["ymax"])
+                bottom_right = (bbox["xmax"], bbox["ymax"])
                 b.create_bbox_from_points(top_left, bottom_right, 
                                           annotation, 
                                           label_id, file_id)
