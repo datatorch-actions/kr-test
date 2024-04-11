3df93e20371a89df5a7dcee3b5d960db8917bb70
Michael Nguyen <ninjeezy@gmail.com>
2024-04-11 14:31:42 -0400

    Update entry.py

--- entry.py
+++ entry.py
@@ -4,5 +4,4 @@ from datatorch import get_input, agent, set_output
 directory = os.path.dirname(os.path.abspath(__file__))
 agent_dir = agent.directories().root
 event = get_input("event")
-
-print(event)
+set_output("returnText",event)
