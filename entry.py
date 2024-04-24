0cd6d081a6edb9f94640edce9dc867d42b97a7e2
Michael Nguyen <ninjeezy@gmail.com>
2024-04-24 13:36:23 -0400

    Update entry.py

--- entry.py
+++ entry.py
@@ -1,6 +1,8 @@
 import os
 
-from datatorch import get_input, agent, set_output
+from datatorch import get_input, agent, set_output, ApiClient
+
+api = ApiClient()
 directory = os.path.dirname(os.path.abspath(__file__))
 agent_dir = agent.directories().root
 
@@ -8,4 +10,35 @@ dt_fileId = get_input("fileId")
 command = get_input("command")
 projectId = get_input("projectId")
 
-set_output("returnText","ok")
+# Query to get file and annotation data to be converted to COCO
+GetNewestExport = """
+  query GetFileData($projectId:ID!, $fileId:String!="0e8ea8d1-6abc-4781-b7be-cee1c65b2949",){
+  projectById(id:$projectId) {
+    files(input: 
+      { 
+      	where: {
+    			id: {
+          	equals:$fileId
+        	}
+    		},
+      	perPage:1,
+      	page: 1
+    	}
+  	) {
+      nodes{
+        id
+        annotations {
+          id
+          metadata
+          labelId
+          sourcesJson
+        }
+      }
+    }
+  }
+}
+"""
+
+raw = api.execute(GetNewestExport, params={"projectId": projectId, "fileId": dt_fileId})
+
+set_output("returnText", raw)
